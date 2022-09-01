863cf72b1ed72c7d8aec50ab09c099b96764f480
nevashe <78653613+Nevashe@users.noreply.github.com>
2022-09-01 16:24:08 +0300

    last

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,19 @@
+create table students
+(
+    id         bigserial primary key,
+    name       varchar(36),
+    age        int
+
+);
+
+insert into students (name, age)
+values ('bob', 21), ('john', 22), ('james', 23), ('alex', 23);
+
+
+
+
+
+
+
+
+
